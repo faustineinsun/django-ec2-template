6d7415b06cd710dca690ef4c9355aff35290c99f
faustineinsun <faustine.yu@gmail.com>
2015-03-28 16:11:43 +0000

    Solved MySQL problem on EC2

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,5 +10,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-
 application = Cling(get_wsgi_application())
